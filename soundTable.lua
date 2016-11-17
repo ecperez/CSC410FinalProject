af84f1413c4e1d56ccd8431b4d880d0e95d5a972
ecperez <ecperez@noctrl.edu>
2016-11-17 10:51:07 -0600

    Delete soundTable.lua

--- soundTable.lua
+++
@@ -1,7 +0,0 @@
-local soundTable = {
-    shootSound = audio.loadSound( "shoot.wav" ),
-    hitSound = audio.loadSound( "hit.wav" ),
-    explodeSound = audio.loadSound( "explode.wav" ),
-}
-
-return soundTable;
